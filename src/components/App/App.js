d9838f7307a1091d97e51ebfde33b1687e15c627
thexyspace <makdaa1@gmail.com>
2018-04-10 20:34:02 +0100

    final push not working run out of time

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,83 +1,90 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import SearchBar from '../components/playlist';
-import SearchResults from '../components/SearchResults';
-import Playlist from '../components/Playlist';
-import Spotify from '../../util/spotify.js';
+import React from 'react'
+import './App.css'
 
-
+import Playlist from '../Playlist/Playlist'
+import SearchBar from '../SearchBar/SearchBar'
+import SearchResults from '../SearchResults/SearchResults'
+import Spotify from '../../util/Spotify'
 
 class App extends Component.React {
-
   constructor(props) {
-    super(props);
-    this.state =
-    {searchResults: tracks},
-    {playlistName: 'Ali Playlist'},
-    {playlistTracks: trackUris },
+    super(props)
 
+    this.state = {
+      searchResults: [],
+      playlistName: 'Ali Playlist',
+      playlistTracks: []
+    }
 
-      this.addTrack = this.addTrack.bind(this);
-      this.removeTrack = this.removeTrack.bind(this);
-      this.updatePlayListName = this.updatePlayListName.bind(this);
-      this.savePlaylist = this.savePlaylist.bind(this);
-      this.search = this.search.bind(this);
-
-//end of constructor method
+    this.search = this.search.bind(this)
+    this.addTrack = this.addTrack.bind(this)
+    this.removeTrack = this.removeTrack.bind(this)
+    this.updatePlaylistName = this.updatePlaylistName.bind(this)
+    this.savePlaylist = this.savePlaylist.bind(this)
   }
 
   addTrack(track) {
-    const tracks = this.state.playlistTracks;
-    if(!this.state.playlistTracks.includes(track.id)) {
-      tracks.push(track);
-      this.setState({playlistTracks: tracks})
+    const tracks = this.state.playlistTracks
+    if (!this.state.playlistTracks.includes(track.id)) {
+      tracks.push(track)
+      this.setState({ playlistTracks: tracks })
     }
   }
 
   removeTrack(track) {
-    let currentTracks = this.state.playlistTracks;
-    let newTracks = currentTracks.filter(currentTrack => currentTrack.id !== track.id);
-    this.setState({playlistTracks: newTracks});
-}
-
-  updatePlayListName(name) {
-    newName = name;
-    this.setState({playListName: newName});
+    let currentTracks = this.state.playlistTracks
+    let newTracks = currentTracks.filter(
+      currentTrack => currentTrack.id !== track.id
+    )
+    this.setState({ playlistTracks: newTracks })
   }
 
+  updatePlayListName(name) {
+    const newName = name
+    this.setState({ playListName: newName })
+  }
 
   savePlaylist() {
-      const trackUris = this.state.playlistTracks.map(track => track.uri);
-      Spotify.savePlaylist(this.state.playlistName, trackUris).then(() => {
-        this.setState({
-          playlistName: 'New Playlist',
-          playlistTracks: []
-        });
-      });
-    }
+    const trackUris = this.state.playlistTracks.map(track => track.uri)
+    Spotify.savePlaylist(this.state.playlistName, trackUris).then(() => {
+      this.setState({
+        playlistName: 'New Playlist',
+        playlistTracks: []
+      })
+    })
+  }
 
-
-  search(name, artist, album) {
+  search(term) {
     Spotify.search(term).then(track => {
-      this.setState({searchResults: track})
+      this.setState({ searchResults: track })
     })
   }
 
   render() {
     return (
       <div>
-        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <h1>
+          Ja<span className="highlight">mmm</span>ing
+        </h1>
         <div className="App">
           <SearchBar onSearch={this.searchbar} />
           <div className="App-playlist">
-            <SearchResults onAdd={this.addTrack} searchResults={this.state.searchResults} />
-            <PlayList onSave={this.savePlaylist} onNameChange={this.updatePlayListName} onRemove={this.removeTrack} playlistName={this.state.playlistName} playlistTrack={this.state.playlistTracks}/>
+            <SearchResults
+              onAdd={this.addTrack}
+              searchResults={this.state.searchResults}
+            />
+            <Playlist
+              onSave={this.savePlaylist}
+              onNameChange={this.updatePlayListName}
+              onRemove={this.removeTrack}
+              playlistName={this.state.playlistName}
+              playlistTrack={this.state.playlistTracks}
+            />
           </div>
         </div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -7,17 +7,18 @@ const Spotify = {
   getAccessToken() {
     if (accessToken) {
       return accessToken;
-    } else if (accessToken && expire_in === url) {
-      return windows.location.href.match(/access_token=([^&]*)/, /expires_in=([^&]*)/);
-      const accessToken = ([^&]*);
-      const expiresIn = ([^&]*)/);
-      const tokenExpire = expiresIn;
+    }
+    const accessTokenValue = windows.location.href.match(/access_token=([^&]*)/);
+    const expirationTime = windows.location.href.match(/expires_in=([^&]*)/);
+    if (accessTokenValue && expirationTime) {
+      accessToken = accessTokenValue[1];
+      return accessToken;
       window.setTimeout(() => accessToken = '', expiresIn * 1000);
       window.history.pushState('Access Token', null, '/');
     } else {
-      windows.location.href(`https://accounts.spotify.com/authorize?client_id=${clientID}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectURI}`)
+      const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientID}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectURI}`
+      window.location = accessUrl;
     }
-  }
 
    search(term) {
      return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`,
@@ -68,58 +69,3 @@ const Spotify = {
 }
 
 export default Spotify;
-
-
-
-return fetch('https://api.spotify.com/v1/me', {headers: headers}
-    ).then(response => response.json()
-    ).then(jsonResponse => {
-      userId = jsonResponse.id;
-      return fetch(`https://api.spotify.com/v1/users/${userId}/playlists`, {
-        headers: headers,
-        method: 'POST',
-        body: JSON.stringify({name: name})
-      }).then(response => response.json()
-      ).then(jsonResponse => {
-        const playlistId = jsonResponse.id;
-        return fetch(`https://api.spotify.com/v1/users/${userId}/playlists/${playlistId}/tracks`, {
-          headers: headers,
-          method: 'POST',
-          body: JSON.stringify({uris: trackUris})
-        });
-      });
-    });
-
-
-    fetch('https://api.spotify.com/v1/me', {
-      {headers: headers}
-    }).then(response => {
-      return response.json();
-    }).then(jsonResponse => {
-      return response.id;
-      let userID = response.id;
-    });
-
-    fetch(`https://api.spotify.com/v1/users/${userId}/playlists`, {
-      method: 'POST',
-      headers: headers,
-      body: name
-    }).then(response => {
-      return response.json();
-    }).then(jsonResponse => {
-      return response.id;
-      let playlistID = response.id;
-    })
-
-    fetch(`https://api.spotify.com/v1/users/${userID}/playlists/${playlistID}/tracks`, {
-      method: 'POST',
-      headers: headers,
-      body: track.uri[],
-    }).then(response => {
-      return response.json();
-    }).then(jsonResponse => {
-      return response.id;
-      let playlistID = response.id;
-    })
-
-    }
